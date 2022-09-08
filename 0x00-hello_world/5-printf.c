4624c2b38135beefc93ddde7718eb01ac3256201
adebayo oyerinmade <oyerinmadea@gmail.com>
2022-09-08 11:01:18 -0700

    work

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -11,10 +11,3 @@ int main(void)
 		printf("%s", "with proper grammar, but the outcome is a piece of art,\n");
 			return (0);
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-
