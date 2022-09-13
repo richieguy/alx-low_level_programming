5dc9d3a994359fe32a16c84d8366448166e63beb
adebayo oyerinmade <oyerinmadea@gmail.com>
2022-09-13 06:24:05 -0700

    new

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,23 +1,23 @@
 #include "main.h"
 
 /**
- *  * main - check the code for Holberton School students.
- *   *
- *    * Return: Always 0.
- */
+*  * main - check the code for Holberton School students.
+*   *
+*    * Return: Always 0.
+*/
 int main(void)
 {
 		print_alphabet();
-			return (0);
+		return (0);
 }
 void print_alphabet(void)
 {
 		char letter = 'a';
 
-			while (letter <= 'z')
-					{
-								_putchar(letter);
-										letter++;
-											}
+		while (letter <= 'z')
+			{
+			_putchar(letter);
+			letter++;
+			}
 				_putchar('\n');
 }
