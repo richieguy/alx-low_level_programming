6007f8e312504adefb0986ed02b7bc82ed62b901
adebayo oyerinmade <oyerinmadea@gmail.com>
2022-09-13 05:07:11 -0700

    stat

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- *  * main - Prints _putchar as a message.
+ * main - Prints _putchar as a message.
  * 
- *  * Return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
-int main(void)
-{
+ int main(void)
+ {
 	int str[] = {95, 112, 117, 116, 99, 104, 97, 114};
 	int count, sz;
 
@@ -16,6 +16,4 @@ int main(void)
 	}
 	putchar('\n');
 	return (0);
-}
-
-
+ }
