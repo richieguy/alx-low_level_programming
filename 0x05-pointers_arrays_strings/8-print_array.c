6cd7005f3d71bbc9e98ac9e2d20140591b46acae
adebayo oyerinmade <oyerinmadea@gmail.com>
2022-09-19 15:36:12 -0700

    new point

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -7,22 +7,21 @@
  *    * @n: int type integer
  *     * Description: Numbers must be separated by comma and space.
  * Numbers should be displayed in the same order they are stored in array.
- *       * You can only use _putchar to print
+ *       * You can only use _putchar to print.
  */
-
 
 void print_array(int *a, int n)
 {
-	int i;
+		int i;
 
-	i = 0;
-	for (n--; n >= 0; n--, i++)
-	{
-	printf("%d", a[i]);
-			if (n > 0)
-	{
-			printf(", ");
-	}
-	}
-		printf("\n")
+		i = 0;
+		for (n--; n >= 0; n--, i++)
+		{
+		printf("%d", a[i]);
+	if (n > 0)
+														{
+																		printf(", ");
+																				}
+												}
+					printf("\n");
 }
