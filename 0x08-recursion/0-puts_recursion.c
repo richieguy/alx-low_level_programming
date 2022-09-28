68ec8cac17abcb844ae914630cc6d3b9e745c374
adebayo oyerinmade <oyerinmadea@gmail.com>
2022-09-28 14:56:14 -0700

    one

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
-* _puts_recursion -> this is a function to print string recursively
+* _puts_recursion -> this is a function to print string recursivly
 * @s: string paramenter
 */
 void _puts_recursion(char *s)
