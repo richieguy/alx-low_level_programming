5fcfc9ff91d12cc949fd8e10c3034736d9d56acf
adebayo oyerinmade <oyerinmadea@gmail.com>
2022-09-29 12:12:55 -0700

    pro

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ *  * find_sqrt - Finds the natural square root of an inputted number.
+ *   * @num: The number to find the square root of.
+ *    * @root: The root to be tested.
+ *     *
+ *      * Return: If the number has a natural square root - the square root.
+ *       *         If the number does not have a natural square root - -1.
+ */
 int root(int n, int ans)
 {
 	if (ans * ans == n)
--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,5 +1,13 @@
 #include "main.h"
 
+/**
+ *  * is_divisible - Checks if a number is divisible.
+ *   * @num: The number to be checked.
+ *    * @div: The divisor.
+ *     *
+ *      * Return: If the number is divisible - 0.
+ *       *         If the number is not divisible - 1.
+ */
 int prime2(int a, int b)
 {
 	if (a == b)
