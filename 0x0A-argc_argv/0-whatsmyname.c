1d683f2ecce1c278137b290b0dd468993d37bd6a
adebayo oyerinmade <oyerinmadea@gmail.com>
2022-10-03 03:55:59 -0700

    agvc

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
- *  * main - print name of program
- *   * @argc: argument counter
- *    * @argv: pointer to array of arguments
- *     * Return: 0 on success
+ * main - print name of program
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
  */
 
 int main(int argc, char *argv[])
--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,10 +2,10 @@
 #include "main.h"
 
 /**
- *  * main - print number of arguments passed to program
- *   * @argc: argument counter
- *    * @argv: pointer to array of arguments
- *     * Return: 0 on success
+ * main - print number of arguments passed to program
+ * @argc: argument counter
+ * @argv: pointer to array of arguments
+ * Return: 0 on success
  */
 
 int main(int argc, char *argv[] __attribute__((unused)))
