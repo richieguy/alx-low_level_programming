1065ab145a489f44ade8e26a4a74a0e830da1e7c
adebayo oyerinmade <oyerinmadea@gmail.com>
2022-10-04 15:44:28 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -13,10 +13,10 @@ char *str_concat(char *s1, char *s2)
 	int i = 0, j = 0, len = 1, i2, j2;
 	char *new;
 
-	if (s1 == NULL && s2 == NULL)
-		return (NULL);
-	if (s1 == NULL || s2 == NULL)
-		return (NULL);
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	while (*(s1 + i))
 	{
 		i++;
