b805e7df16d100fbd0f7445b35a95ea27b926eee
adebayo oyerinmade <oyerinmadea@gmail.com>
2022-10-21 00:03:54 -0700

    single

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,29 +1,33 @@
-#ifndef _LISTS_
-#define _LISTS_
+#ifndef MAIN_H
+#define MAIN_H
 
-#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 /**
- * string list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string 
- * @next: point to the next mode 
+ * struct list_s - singly linked list
  *
- * Description: singly list string mode structure 
+ * @str: string - (malloc'ed string)
+ *
+ * @len: length of the string
+ *
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ *
+ * for ALX project
  */
+
 typedef struct list_s
 {
 	char *str;
 	unsigned int len;
-	struct int list_s *next;
+	struct list_s *next;
 } list_t;
 
-size_t print _list(const list_t *h);
+size_t print_list(const list_t *h);
 size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
-list_t *add_node_end(list_t **head const char *str);
+list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
 #endif
